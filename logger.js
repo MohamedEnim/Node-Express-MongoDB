d3b48c4fd37fa9bdf966e3ba52599a3a329960d3
MohamedEnim <medbentaher.enim@gmail.com>
2020-09-29 13:12:11 +0200

    add new router

--- logger.js
+++ logger.js
@@ -10,7 +10,7 @@ router.get("/", async (req, res) => {
 	console.log(courses);
 });
 
-router.post("/c1", async (req, res) => {
+router.post("/", async (req, res) => {
 	const course = req.body;
 	const c1 = new Course({
 		tags: course.tags,
@@ -20,6 +20,48 @@ router.post("/c1", async (req, res) => {
 	});
 	const saveCourse = await c1.save();
 	res.status(201).send(saveCourse);
+});
+
+router.get("/:id", async (req, res) => {
+	const id = req.params.id;
+	const course = await Course.findById(id);
+	//
+	res.status(200).send(course);
+});
+
+router.put("/:id", async (req, res) => {
+	const id = req.params.id;
+	const data = req.body;
+	/*result = await Course.update(
+		{ _id: id },
+		{
+			$set: {
+				name: data.name,
+				isPublished: data.isPublished,
+			},
+		}
+	);
+	res.send(result);*/
+	const course = await Course.findByIdAndUpdate(
+		id,
+		{
+			$set: {
+				name: data.name,
+				isPublished: data.isPublished,
+			},
+		},
+		{
+			new: true,
+		}
+	);
+	res.send(course);
+});
+
+router.delete("/:id", async (req, res) => {
+	const id = req.params.id;
+
+	const result = await Course.deleteOne({ _id: id });
+	res.send(result);
 });
 
 module.exports = router;
